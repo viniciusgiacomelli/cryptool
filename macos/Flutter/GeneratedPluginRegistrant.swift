4dee335fdd83e11b55af7f05668267116b86c8b6
Vinicius <giacomelli.vinicius@gmail.com>
2024-05-07 16:53:37 -0300

    feat: :package: Dependencies added to project

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import fast_rsa
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FastRsaPlugin.register(with: registry.registrar(forPlugin: "FastRsaPlugin"))
 }
