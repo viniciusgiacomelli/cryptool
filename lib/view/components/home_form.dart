25b7e5da2e3fe47e34c6c84ee654fe9596c49763
Vinicius <giacomelli.vinicius@gmail.com>
2024-05-08 09:49:40 -0300

    progress: :construction: Rows constraints

--- lib/view/components/home_form.dart
+++ lib/view/components/home_form.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 class HomeForm extends StatefulWidget {
@@ -19,89 +19,18 @@ class _HomeFormState extends State<HomeForm> {
   Widget build(BuildContext context) {
     return Form(
         key: _formKey,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              flex: 2,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
                 children: [
-                  Text("Texto claro"),
-                  TextFormField(
-                    maxLines: 6,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "segredo",
-                        contentPadding: EdgeInsets.symmetric(
-                            vertical: 12,
-                            horizontal: 8
-                        )
-                    ),
-                    controller: secretPhraseController,
-                  ),
-                  SizedBox(height: 10,),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                        onPressed: (){},
-                        child: Text("Carregar ... ")
-                    ),
-                  ),
-                  SizedBox(height: 10,),
-                  TextFormField(
-                    maxLines: 6,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "Chave Publica",
-                        contentPadding: EdgeInsets.symmetric(
-                            vertical: 12,
-                            horizontal: 8
-                        )
-                    ),
-                    controller: secretPhraseController,
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Text("Algoritmo"),
-                  Icon(Icons.arrow_forward_rounded),
-                  SizedBox(height: 200,),
-                  ElevatedButton(
-                    style: ButtonStyle(
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                                borderRadius: BorderRadius.zero,
-                                side: BorderSide(color: Colors.red)
-                            )
-                        )
-                    ),
-                    onPressed: (){},
-                    child: Text("Gerar novo par de chaves")
-                  )
-                ],
-              ),
-            ),
-            Expanded(
-              flex: 2,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text("Texto criptografado"),
-                    TextFormField(
-                      maxLines: 6,
-                      decoration: InputDecoration(
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
                           border: OutlineInputBorder(),
-                          hintText: "s@e%4Fg_R54rgT",
+                          labelText: "Texto claro",
+                          hintText: "Insira seu texto",
                           contentPadding: EdgeInsets.symmetric(
                               vertical: 12,
                               horizontal: 8
@@ -109,37 +38,20 @@ class _HomeFormState extends State<HomeForm> {
                       ),
                       controller: secretPhraseController,
                     ),
-                    SizedBox(height: 10,),
-                    SizedBox(
-                      child: Container(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Expanded(
-                                child: ElevatedButton(
-                                    onPressed: (){},
-                                    child: Text("Download")
-                                ),
-                              ),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              Expanded(
-                                child: ElevatedButton(
-                                    onPressed: (){},
-                                    child: Text("Copiar")
-                                ),
-                              ),
-                            ],
-                          ),
-                      ),
-                    ),
-                    SizedBox(height: 10,),
-                    TextFormField(
-                      maxLines: 6,
-                      decoration: InputDecoration(
+                  ),
+                  Expanded(
+                      child: Column(
+                        children: [
+                          Text("algoritmo"),
+                        ],
+                      )
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
                           border: OutlineInputBorder(),
-                          labelText: "Chave Privada",
+                          labelText: "Texto criptografado",
+                          hintText: "O texto criptografado aparecerá aqui",
                           contentPadding: EdgeInsets.symmetric(
                               vertical: 12,
                               horizontal: 8
@@ -147,10 +59,74 @@ class _HomeFormState extends State<HomeForm> {
                       ),
                       controller: secretPhraseController,
                     ),
-                  ],
-                )
-            )
-          ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 16,),
+              Row(
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        ElevatedButton(onPressed: (){}, child: Text("Procurar")),
+                      ],
+                    )
+                  ),
+                  Expanded(
+                      child: Column(
+                        children: [
+                          Text("algoritmo"),
+                        ],
+                      )
+                  ),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        ElevatedButton(onPressed: (){}, child: Text("Download")),
+                        ElevatedButton(onPressed: (){}, child: Text("Copiar"))
+                      ],
+                    )
+                  ),
+                ],
+              ),
+              SizedBox(height: 16,),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "keyId",
+                          hintText: "Key ID:",
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 12,
+                              horizontal: 8
+                          )
+                      ),
+                      controller: secretPhraseController,
+                    ),
+                  ),
+                  Expanded(
+                      child: Text("Text")
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "keyId",
+                          hintText: "Key ID:",
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 12,
+                              horizontal: 8
+                          )
+                      ),
+                      controller: secretPhraseController,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         )
     );
   }
--- lib/view/pages/homepage.dart
+++ lib/view/pages/homepage.dart
@@ -1,24 +1,23 @@
 import 'package:cryptool/view/components/home_form.dart';
 import 'package:cryptool/view/components/home_header.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              HomeHeader(),
-              HomeForm()
-            ],
-          ),
+        child: Column(
+          children: [
+            HomeHeader(),
+            HomeForm()
+          ],
         ),
-      ),
+      )
     );
   }
 
